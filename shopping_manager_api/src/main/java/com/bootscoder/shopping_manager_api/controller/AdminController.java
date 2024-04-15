319b614791af1df8895ea8ccc81a5373f037aaf1
coderBoots <jiahaox@chanjet.com>
2024-04-15 21:20:29 +0800

    这个异常我想让我的异常捕获器捕捉到该怎么办呢

---
+++ shopping_manager_api/src/main/java/com/bootscoder/shopping_manager_api/controller/AdminController.java
@@ -0,0 +1,54 @@
+package com.bootscoder.shopping_manager_api.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.bootscoder.shopping_common.pojo.Admin;
+import com.bootscoder.shopping_common.result.BaseResult;
+import com.bootscoder.shopping_common.service.AdminService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 管理员api
+ *
+ * @author bootsCoder
+ * @date created on 2024/4/15
+ */
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @DubboReference
+    private AdminService adminService;
+
+    @PostMapping("/add")
+    public BaseResult add(@RequestBody Admin admin) {
+        adminService.add(admin);
+        return BaseResult.ok();
+    }
+    @PutMapping("/update")
+    public BaseResult update(@RequestBody Admin admin) {
+        adminService.update(admin);
+        return BaseResult.ok();
+    }
+    @DeleteMapping("/delete")
+    public BaseResult delete(@RequestParam Long id) {
+        adminService.delete(id);
+        return BaseResult.ok();
+    }
+    @GetMapping("/findById")
+    public BaseResult<Admin> findById(Long aid) {
+        Admin admin = adminService.findById(aid);
+        return BaseResult.ok(admin);
+    }
+    @GetMapping("/search")
+    public BaseResult<Page<Admin>> search(int page, int size) {
+        Page<Admin> adminPage = adminService.search(page, size);
+        return BaseResult.ok(adminPage);
+    }
+    @PutMapping("/updateRoleToAdmin")
+    public BaseResult updateRoleToAdmin( @RequestParam("aid") Long aid, @RequestParam("rids") Long[] rids) {
+        adminService.updateRoleToAdmin(aid,rids);
+        return BaseResult.ok();
+    }
+}
