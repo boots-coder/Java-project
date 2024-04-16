54739219b80d002d43dca4de31d3d2464b7b3801
coderBoots <jiahaox@chanjet.com>
2024-04-16 21:53:41 +0800

    角色和权限相关所有接口ping通

--- shopping_admin_service/src/main/java/com/bootscoder/shopping_admin_service/mapper/PermissionMapper.java
+++ shopping_admin_service/src/main/java/com/bootscoder/shopping_admin_service/mapper/PermissionMapper.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.bootscoder.shopping_common.pojo.Permission;
 
 public interface PermissionMapper extends BaseMapper<Permission> {
-    // 删除角色_权限表中的相关数据
+    /**
+     * 删除角色_权限表中的相关数据
+     * @param pid 权限id
+     */
     void deletePermissionAllRole(Long pid);
 }
