b602065d229e8d617bbaa63f582212290aa8f072
coderBoots <jiahaox@chanjet.com>
2024-04-18 17:28:25 +0800

    成功完成权限不足测试

--- shopping_manager_api/src/main/java/com/bootscoder/shopping_manager_api/security/AccessDeniedExceptionHandler.java
+++ shopping_manager_api/src/main/java/com/bootscoder/shopping_manager_api/security/AccessDeniedExceptionHandler.java
@@ -1,10 +1,9 @@
 package com.bootscoder.shopping_manager_api.security;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.nio.file.AccessDeniedException;
-import java.rmi.AccessException;
 
 /**
  * 统一异常处理
@@ -16,8 +15,7 @@ import java.rmi.AccessException;
 public class AccessDeniedExceptionHandler {
     // 处理权限不足异常，捕获到异常后再次抛出，交给AccessDeniedHandler处理
     @ExceptionHandler(AccessDeniedException.class)
-    public void defaultExceptionHandler(AccessDeniedException e) throws
-             AccessDeniedException {
+    public void defaultExceptionHandler(AccessDeniedException e) throws AccessDeniedException{
         throw e;
     }
 }
