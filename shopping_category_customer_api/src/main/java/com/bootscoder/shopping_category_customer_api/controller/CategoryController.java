21a806c5e5542c33875a743b959a1c59fcf3e505
coderBoots <jiahaox@chanjet.com>
2024-04-23 16:26:48 +0800

    创建前台用户管理的服务和接口（注释redis和实现类）

---
+++ shopping_category_customer_api/src/main/java/com/bootscoder/shopping_category_customer_api/controller/CategoryController.java
@@ -0,0 +1,36 @@
+package com.bootscoder.shopping_category_customer_api.controller;
+
+import com.bootscoder.shopping_common.pojo.Category;
+import com.bootscoder.shopping_common.result.BaseResult;
+import com.bootscoder.shopping_common.service.CategoryService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
+import java.util.List;
+
+/**
+ * 广告
+ */
+@RestController
+@RequestMapping("/user/category")
+public class CategoryController {
+    @DubboReference
+    private CategoryService categoryService;
+
+    @GetMapping("/all")
+    public BaseResult<List<Category>> findAll(){
+        List<Category> categories = categoryService.findAll();
+        return BaseResult.ok(categories);
+    }
+
+    @GetMapping("/test")
+    public BaseResult test(HttpServletRequest request){
+        Enumeration<String> headerNames = request.getHeaderNames();
+        return BaseResult.ok();
+    }
+
+}
